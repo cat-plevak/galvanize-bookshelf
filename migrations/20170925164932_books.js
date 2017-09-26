c288257fb4d625d417a26bb7cf4cba59648c343f
cat-plevak <catherine.plevak@colorado.edu>
2017-09-26 09:58:30 -0600

    seed works, weird test error though

--- migrations/20170925164932_books.js
+++ migrations/20170925164932_books.js
@@ -1,15 +1,15 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("books", function(table) {
+  return knex.schema.createTable('books', function(table) {
     table.increments();
-    table.varchar("title", 255).notNullable().default("");
-    table.varchar("author", 255).notNullable().default("");
-    table.varchar("genre", 255).notNullable().default("");
-    table.text("description").notNullable().default("");
-    table.text("cover_url").notNullable().default("");
+    table.varchar('title', 255).notNullable().default("");
+    table.varchar('author', 255).notNullable().default("");
+    table.varchar('genre', 255).notNullable().default("");
+    table.text('description').notNullable().default("");
+    table.text('cover_url').notNullable().default("");
     table.timestamps(true, true);
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("todo");
+  return knex.schema.dropTable('books');
 };
