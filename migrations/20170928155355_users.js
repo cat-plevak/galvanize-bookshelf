94fb2591a722faf3859f05592068f43681f87448
cat-plevak <catherine.plevak@colorado.edu>
2017-09-30 16:08:47 -0600

    part 4

---
+++ migrations/20170928155355_users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.varchar('first_name', 255).notNullable().default("");
+    table.varchar('last_name', 255).notNullable().default("");
+    table.varchar('email', 255).notNullable().unique();
+    table.specificType('hashed_password', 'CHAR(60)').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,35 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // YOUR CODE HERE
+router.post('/users', function(req, res, next) {
+  const password = req.body.password;
+  bcrypt.hash(password, 12)
+    .then((hashedPassword) => {
+      return knex('users')
+        .insert ({
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          hashed_password: hashedPassword
+        }, '*')
+    })
+    .then((user) => {
+      const response = {
+        id: user[0].id,
+        firstName: user[0].first_name,
+        lastName: user[0].last_name,
+        email: user[0].email
+      }
+      res.send(response);
+      console.log(response)
+    })
+    .catch((err) => next(err));
+})
 
 module.exports = router;
