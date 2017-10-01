29edfba25963166094d616167f5bce11bf0ef494
cat-plevak <catherine.plevak@colorado.edu>
2017-10-01 17:12:05 -0600

    part 5 migration

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const boom = require('boom');
+const bcrypt = require('bcrypt-as-promised');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
